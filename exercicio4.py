5d41bb64d36e6549fc27ad31af8b3bf02d97d489
jhenrilima27-web <jhenrilima27@gmail.com>
2026-03-30 12:46:37 -0300

    Exercicios do 1 ao 4

---
+++ exercicio4.py
@@ -0,0 +1,4 @@
+test = input("Digite qualquer coisa ")
+print('é numerico? {}'.format(test.isnumeric()))
+print('É alfabetico? {}'.format(test.isalpha()))
+print('É Alfanumerico ? {}'.format(test.isalnum()))
