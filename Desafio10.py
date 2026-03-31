b2f296af2d4abed2fa224b5f51588ac900d77946
jhenrilima27-web <jhenrilima27@gmail.com>
2026-03-31 15:24:10 -0300

    Desafio 11

--- Desafio10.py
+++ Desafio10.py
@@ -1,3 +1,3 @@
 # Desafio 10
-dinheiro = float(input('Quanto dinheiro vc tem na carteira ? R$'))
-print ('Com R${} voce pode comprar US${}'.format(dinheiro,dinheiro/5.3))
+dinheiro = float(input('Quanto dinheiro vc tem na carteira ? R$ '))
+print ('Com R${:.2f} voce pode comprar US${:.2f}'.format(dinheiro,dinheiro/5.3))
