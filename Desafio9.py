60b24ac0bd58dbb20c9185dfea17adf7fe9dc0d2
jhenrilima27-web <jhenrilima27@gmail.com>
2026-03-31 15:06:28 -0300

    Desafios do 1 ao 9

---
+++ Desafio9.py
@@ -0,0 +1,5 @@
+tabuada = int(input('digite um numero para ver a sua tabuada: '))
+print ('-'*12)
+print('{} x  1 = {} \n{} x  2 = {} \n{} x  3 = {} \n{} x  4 = {} \n{} x  5 = {}'.format(tabuada,tabuada,tabuada,tabuada*2,tabuada,tabuada*3,tabuada,tabuada*4,tabuada,tabuada*5))
+print('{} x  6 = {} \n{} x  7 = {} \n{} x  8 = {} \n{} x  9 = {} \n{} x 10 = {}'.format(tabuada,tabuada*6,tabuada,tabuada*7,tabuada,tabuada*8,tabuada,tabuada*9,tabuada,tabuada*10))
+print ('-'*12)
