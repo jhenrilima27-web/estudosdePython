9e0c7538d8c53d4ec28dfc1cb90acb0afef05c95
jhenrilima27-web <jhenrilima27@gmail.com>
2026-04-01 13:30:09 -0300

    Desafio 11 e 12

--- Desafio11.py
+++ Desafio11.py
@@ -1,5 +1,5 @@
-largura = float(input('Largura da parede'))
-altura = float(input('Altura da parede'))
+largura = float(input('Largura da parede: '))
+altura = float(input('Altura da parede: '))
 area = altura*largura
 print ('Sua parede tem a dimensão de {}x{} e sua área é de {}m^2'.format(largura,altura,area))
-print ('Para pintar essa parede, você precisará de {}l de linta'.format(area/2))
+print ('Para pintar essa parede, você precisará de {}l de Tinta'.format(area/2))
---
+++ Desafio12.py
@@ -0,0 +1,3 @@
+preço = float(input('qual o preço do produto ? R$'))
+novo = preço*(1-0.05)
+print ('O preço do produto era R${}, com 5% de desconto será R${}'.format(preço,novo))
