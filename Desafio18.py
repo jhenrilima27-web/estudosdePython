8c0bc27738dadc46db665bd81a8824341349e18d
jhenrilima27-web <jhenrilima27@gmail.com>
2026-04-28 12:52:52 -0300

    Desafio 18

---
+++ Desafio18.py
@@ -0,0 +1,5 @@
+import math
+ang = int(input('Digite um angulo'))
+print ('O seno do angulo é {:.2f}'.format(math.sin(math.radians(ang))))
+print ('O coseno do angulo é {:.2f}'.format(math.cos(math.radians(ang))))
+print ('A tangente do angulo é {:.2f}'.format(math.tan(math.radians(ang))))
