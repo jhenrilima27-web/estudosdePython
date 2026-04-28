e350ddc2571fa79e92b8d8361a20b829ace49813
jhenrilima27-web <jhenrilima27@gmail.com>
2026-04-28 13:07:22 -0300

    Desafio 20

---
+++ Desafio20.py
@@ -0,0 +1,9 @@
+import random
+aluno5 = input('Nome do primeiro aluno: ')
+aluno6 = input('nome do segunndo aluno: ')
+aluno7 = input('nome do terceiro aluno: ')
+aluno8 = input('nome do quarto aluno: ')
+lista1 = [aluno5,aluno6,aluno7,aluno8]
+random.shuffle(lista1)
+print ('A ondem será')
+print (lista1)
