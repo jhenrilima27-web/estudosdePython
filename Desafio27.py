3a4e7d9415931bd3d75e9dfa9142c32159826926
jhenrilima27-web <jhenrilima27@gmail.com>
2026-05-14 16:59:04 -0300

    Desafio 27

---
+++ Desafio27.py
@@ -0,0 +1,5 @@
+# Desafio 27
+nomer = str(input('Digite seu nome completo: ')).strip()
+listan = nomer.split()
+print ('Seu primeiro nome é {} o ultimo é {}'.format(listan[0],listan[len(listan)-1]))
+#print ('o Ultimo nome é {}'.format(listan[len(listan)-1]))
